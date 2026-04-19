3001bb0ed155ce406d52a18118d39b1395f4b1a4
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2026-04-18 20:38:06 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 
-Route::get('/', [ProductoController::class, 'index']);
+//Route::get('/', [ProductoController::class, 'index']);
+
+Route::get('/', function () {
+    return "HELLO RAILWAY";
+});
