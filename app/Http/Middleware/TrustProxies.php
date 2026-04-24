a29bcd5d4521ba4ba66a86784d50a22f469b4db8
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2026-04-24 16:28:13 -0500

    https fix midlw

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Confiar en todos los proxies (Railway)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Usar todos los headers forward
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
