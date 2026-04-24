2ce39ef223858a07ec9343ca3b806757b694da34
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2026-04-24 14:16:34 -0500

    https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Services\CustomSqlServerConnector;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
